2a2f4d7937e3b8a86d721bdab010ff3dd6ed0885
qw86516 <718599112@qq.com>
2019-07-30 09:44:17 +0800

    dubbo manager

--- dubbo_provider/src/main/java/com/demo/service/impl/RpcEdocServiceImpl.java
+++ dubbo_provider/src/main/java/com/demo/service/impl/RpcEdocServiceImpl.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-
 @Service
 @Component
 public class RpcEdocServiceImpl implements RpcEdocService{
